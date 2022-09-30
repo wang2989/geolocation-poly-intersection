28ca9f9e259bc18eca34b56e3ccc6d30e2d077e7
Ziheng Cai <70208314+LeonCai1@users.noreply.github.com>
2022-09-30 16:08:50 -0400

    Tested two algorithms and they should work for now

--- point_in_poly/algorithms/ray_casting.py
+++ point_in_poly/algorithms/ray_casting.py
@@ -1,8 +1,17 @@
-from json.encoder import INFINITY
-
-
+MAX = 1000
+def pip(points: list, v: list) -> list:
+    res =[]
+    for point in points: 
+        if (is_inside_polygon(point, v)):
+            res.append(point)
+    return res
+# p1 = (-122.0045, 40.0335)
+# p2 = (-122.0625, 39.60383)
+# p3 = (-122.992, 38.90133)
+# v=[[-123.15532480599389,39.628538026418425],[-119.0541702886981,39.40845421648897],[-122.53717977729988,37.92347617009539],[-123.6189335775142,39.160027074679334],[-123.15532480599389,39.628538026418425]]
+#v = [[-123.26039217084163,39.53042492619664],[-118.31546219912128,39.540567692544926],[-123.01051539035592,38.147591729521345],[-117.9998283711392,36.906482297387285],[-121.01150114646877,40.086073672949816],[-121.95840263041526,37.21083867441126],[-118.30231078962211,38.3438355532667],[-120.66956449948808,38.85773081562929],[-123.26039217084163,39.53042492619664]]
 def is_inside_polygon(p: tuple, v: list) -> bool:
-    extreme = [INFINITY, p[1]]
+    extreme = [MAX, p[1]]
     n = len(v)
     count = i =0
     decrease =0
@@ -48,7 +57,11 @@ def direction(p1: tuple, p2:tuple, p3:tuple)-> int:
        return 1
    return 2
 def onSegment(p1: tuple, p2:tuple, p3:tuple)-> bool:
-    if( p2[0] >= min( p1[0], p3[0]) & p2[0]<= max(p1[0], p3[0])
-       & p2[1] >= min( p1[1], p3[1]) & p2[1]<= max(p1[1], p3[1])):
+    if( (p2[0] >= min( p1[0], p3[0])) & (p2[0]<= max(p1[0], p3[0]))
+       & (p2[1] >= min( p1[1], p3[1])) & (p2[1]<= max(p1[1], p3[1]))):
         return True
     return False
+
+# print(is_inside_polygon(p1, v))
+# print(is_inside_polygon(p2, v))
+# print(is_inside_polygon(p3, v))
--- point_in_poly/point_in_poly.py
+++ point_in_poly/point_in_poly.py
@@ -2,12 +2,13 @@ import algorithms.ray_casting as rc
 import algorithms.wind_number as wn
 
 class PointInPoly:
-    def __init__(self, points:tuple, v: list) -> None:
+    def __init__(self, points: list, v: list) -> None:
         self.points = points
         self.v = v
         
     def rayCasting(self) -> list:
-        return rc(self.points, self.v)
+        
+        return rc.pip(self.points, self.v)
     
     def windNumber(self) -> list:
-        return wn(self.points, self.v )
+        return wn.pip(self.points, self.v )
